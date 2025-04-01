0b1dec1c72ddbc5eef49770e9b37f1ee1a985401
NAM-IL <newlaser@hanmail.net>
2025-04-01 09:27:12 +0900

    04월 01일(화)

---
+++ code/Klass.py
@@ -0,0 +1,13 @@
+class Klass:
+    # 생성자
+    def __init__(self):
+        pass
+        
+    # 인스턴스 메서드 정의
+    def some_method(self):
+        pass
+    
+    
+class MyClass:
+    def __init__(self):
+        pass
